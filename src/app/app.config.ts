80825396abca3c471743d446e5c879356b90a914
ianlabicani <labicaniian@gmail.com>
2024-01-27 20:28:58 +0800

    fix(config): :bug: provide an object to avoid errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,11 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
     provideRouter(routes),
     provideAnimations(),
     importProvidersFrom(
