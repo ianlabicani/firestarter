0a32a812f39ef45280b0a546440c854d4fdba8d0
ianlabicani <labicaniian@gmail.com>
2024-01-27 20:44:41 +0800

    feat(component): :sparkles: create login button for google signin

--- src/app/user/login-page/login-page.component.ts
+++ src/app/user/login-page/login-page.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GoogleSigninDirective } from '../google-signin.directive';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GoogleSigninDirective],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-
+  afAuth = inject(AngularFireAuth);
 }
