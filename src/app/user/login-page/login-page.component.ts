4272e36109c2e1c05af3785ed2023b5618293fd6
ianlabicani <labicaniian@gmail.com>
2024-01-27 20:56:14 +0800

    feat(component): :package: import shared module

--- src/app/user/login-page/login-page.component.ts
+++ src/app/user/login-page/login-page.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { GoogleSigninDirective } from '../google-signin.directive';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, GoogleSigninDirective],
+  imports: [CommonModule, GoogleSigninDirective, SharedModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
