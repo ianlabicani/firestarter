f784f3c88953ee50efc2e970a79c4f4144a2f382
ianlabicani <labicaniian@gmail.com>
2024-01-28 17:57:02 +0800

    feat(component): :sparkles: use amail-login component

--- src/app/user/login-page/login-page.component.ts
+++ src/app/user/login-page/login-page.component.ts
@@ -3,11 +3,17 @@ import { GoogleSigninDirective } from '../google-signin.directive';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { EmailLoginComponent } from '../email-login/email-login.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, GoogleSigninDirective, SharedModule],
+  imports: [
+    CommonModule,
+    GoogleSigninDirective,
+    SharedModule,
+    EmailLoginComponent,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
