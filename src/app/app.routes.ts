cb1b3a1a74ba8df93ee6303ab479be432ae71be7
ianlabicani <labicaniian@gmail.com>
2024-11-25 14:00:58 +0800

    Create Login Page UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./user/login-page/login-page.component').then(
+        (c) => c.LoginPageComponent
+      ),
+  },
+];
---
+++ src/app/user/login-page/login-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  FormsModule,
+  FormControl,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+})
+export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+  loginForm = this.fb.group({
+    emial: this.emailFormControl,
+    password: this.passwordFormControl,
+  });
+
+  onSubmit() {
+    // TODO: login user with firebase
+    console.log(this.loginForm.getRawValue());
+  }
+}
